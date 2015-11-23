306d2b4882980fb7a39aae8d34f0167b85c0227f
justin <justin.schulz@gmail.com>
2015-11-23 10:41:32 -0500

    Auth modal starts hidden

--- app/assets/javascripts/components/auth_modal.js.jsx
+++ app/assets/javascripts/components/auth_modal.js.jsx
@@ -1,6 +1,9 @@
 window.AuthModal = React.createClass({
   getInitialState: function () {
-    return ({ signIn: true });
+    return ({
+      signIn: true,
+      hidden: true
+    });
   },
 
   formTypeChanged: function () {
@@ -9,9 +12,10 @@ window.AuthModal = React.createClass({
 
   render: function () {
     var form = (this.state.signIn ? <SessionForm /> : <RegistrationForm />);
+    var hidden = (this.state.hidden ? " hidden" : "");
 
     return (
-      <div className="auth-modal group">
+      <div className={ "auth-modal group" + hidden }>
         <fieldset className="group">
           <label className="radio-label">
             <input
