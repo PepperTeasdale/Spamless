a9de90c59924a6ef6d26f9bc15c89d558399f0ec
App Academy Student <student@appacademy.io>
2015-11-26 07:11:45 -0500

    Some styling.

--- app/assets/javascripts/components/landing-page/landing_page.js.jsx
+++ app/assets/javascripts/components/landing-page/landing_page.js.jsx
@@ -6,6 +6,9 @@ LandingPage = React.createClass({
   render: function () {
     return (
       <div className="landing-page group">
+        <h1 className="landing-page-header">
+          Order delivery from 100% Spam-free local restaurants
+        </h1>
         <Navbar landingPage={ true } />
         <SearchForm redirect={ this.redirect } />
       </div>
